2259905be9f001e30f2cae470ab1e897b398a697
Azhang9328 <azhang9328@gmail.com>
2020-02-13 12:50:47 -0800

    fixed suggestion creation

--- skynet-suggests-backend/app/models/repo.rb
+++ skynet-suggests-backend/app/models/repo.rb
@@ -12,10 +12,6 @@ class Repo < ApplicationRecord
         self.update(bundle_id: repo["bundleId"], analysis_status: "bundle created")
   end
 
-  #TO DO LIST 
-  #- ANALYSIS CANNOT RUN IF NO BUNDLE
-  #- ANALYSIS MUST ACCOUNT FOR DEEPCODE RETURNING DIFF STAGES
-  #  CANNOT CREATE SUGGESTION IF STAGE != DONE
   def get_analysis #possibly use time stamps + marker when creating and locating suggestions ex. Suggestion where marker = 0 && time_stamp within last 3 min
     response = RestClient.get("www.deepcode.ai/publicapi/analysis/#{self.bundle_id}", headers={"Session-Token": "338e2a442e9f189f8ca3705c97f5343ddcf5eec225d749398b5bd3f7a6f662a3"})
     parsed_response = JSON.parse(response)
@@ -24,34 +20,35 @@ class Repo < ApplicationRecord
     when "FETCHING", "ANALYZING", "DC_DONE"
       self.update(analysis_status: "analysis in progress")
     when "DONE"
+    ActiveRecord::Base.logger.silence do
       repo_files = parsed_response["analysisResults"]["files"] 
       files_arr = repo_files.keys #files that have suggestions
-      files_arr.length.times do |i| #loop over files
-        file = files_arr[i] 
-        marker_arr = repo_files["#{files_arr[i]}"].keys #marker matches error to suggestion
-          marker_arr.length.times do |x| #for every marker loop once
-            marker = marker_arr[i] 
-            highlight_arr = repo_files["#{files_arr[i]}"]["#{marker_arr[x]}"]
-            highlight_arr.length.times do |i| #loop over code areas to highlight within each marker
-              rows = highlight_arr[i]["rows"]
-              cols = highlight_arr[i]["cols"]
-              Suggestion.create(rows: rows, cols: cols, file: file, marker: marker, repo_id: self.id)
+      files_arr.each do |file| #loop over files
+        marker_arr = repo_files["#{file}"].keys #marker matches error to suggestion
+        puts "#{marker_arr}--------------------------------------------"
+          marker_arr.each do |marker| #for every marker loop once
+            puts "--#{marker}"
+            highlight_arr = repo_files["#{file}"]["#{marker}"]
+            highlight_arr.each do |highlight| #loop over code areas to highlight within each marker
+              rows = highlight["rows"]
+              cols = highlight["cols"]
+              puts Suggestion.create(rows: rows, cols: cols, file: file, marker: marker, repo_id: self.id)
               #create half of suggestion with data from files
             end
           end
       end
       repo_suggestions = parsed_response["analysisResults"]["suggestions"]
-      suggestions_arr = repo_suggestions.keys
-      suggestions_arr.length.times do |i|
-        matching_marker = suggestions_arr[i] #marker matching suggestion to error
-        key_arr = repo_suggestions["#{suggestions_arr[i]}"].keys #[id, message, severity]
-        id = repo_suggestions["#{suggestions_arr[i]}"]["#{key_arr[0]}"]
-        message = repo_suggestions["#{suggestions_arr[i]}"]["#{key_arr[1]}"]
-        severity = repo_suggestions["#{suggestions_arr[i]}"]["#{key_arr[2]}"]
-        Suggestion.where("marker = ?", matching_marker.to_i).each do |suggestion| #find and loop over suggestions with matching marker 
-          suggestion.update(dp_id: id, message: message, severity: severity, marker: nil)
+      marker_arr = repo_suggestions.keys
+      marker_arr.each do |matching_marker|
+        key_arr = repo_suggestions["#{matching_marker}"].keys #[id, message, severity]
+        id = repo_suggestions["#{matching_marker}"]["#{key_arr[0]}"]
+        message = repo_suggestions["#{matching_marker}"]["#{key_arr[1]}"]
+        severity = repo_suggestions["#{matching_marker}"]["#{key_arr[2]}"]
+        Suggestion.where("marker = ?", matching_marker.to_i).each do |suggestion| #find and loop over suggestions with matching marker
+          suggestion.update(dp_id: id, message: message, severity: severity)
           #fill in other half of suggestion with data from suggestions and set marker to nil
         end
+      end
       end
       self.update(analyzed: true, analysis_status: nil)
     when "FAILED"
--- skynet-suggests-backend/db/seeds.rb
+++ skynet-suggests-backend/db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 u1 = User.create(name: "Allen")
 u2 = User.create(name: "Eli")
-
 r1 = Repo.create(nickname: "TestRepo1", url: "https://github.com/azhang9328/skynet-suggests", user_id: User.first.id, bundle_id: "gh/azhang9328/skynet-suggests/4095f9a70a0d7b3ec97d4c5548ecc0c0fbfe942b" )
+r2 = Repo.create(nickname: "Christina", url: "https://github.com/christinamcmahon/travel-pin", user_id: User.first.id, bundle_id: "gh/christinamcmahon/travel-pin/a5ed7ad95d82842e9f3fe4933c15982d35ad07a2")
