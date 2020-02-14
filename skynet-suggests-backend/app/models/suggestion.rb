60eebd2b5305dbe0e047dae41d6904069a6875e7
Azhang9328 <azhang9328@gmail.com>
2020-02-13 17:57:13 -0800

    relationship fixed

--- skynet-suggests-backend/app/models/suggestion.rb
+++ skynet-suggests-backend/app/models/suggestion.rb
@@ -1,3 +1,4 @@
 class Suggestion < ApplicationRecord
     belongs_to :repo
+    belongs_to :user
 end
--- skynet-suggests-backend/app/serializers/user_serializer.rb
+++ skynet-suggests-backend/app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name
   has_many :repos
+  has_many :suggestions, through: :repos
 end
